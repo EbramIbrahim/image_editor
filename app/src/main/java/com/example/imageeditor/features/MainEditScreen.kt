3ddcd1dc227cc329fe62f7db83b45bfbeb2d8e3d
EbramIbrahim <104721363+EbramIbrahim@users.noreply.github.com>
2025-07-09 15:12:47 +0300

    main edit screen to pick image and navigate to specific edit section

---
+++ app/src/main/java/com/example/imageeditor/features/MainEditScreen.kt
@@ -0,0 +1,223 @@
+package com.example.imageeditor.features
+
+import android.Manifest
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.imageeditor.R
+import com.example.imageeditor.utils.Utils
+import java.io.FileDescriptor
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainEditScreen(
+    image: ImageBitmap?
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.White),
+                title = {
+                    Row {
+                        Text(
+                            "Edit Image",
+                            style = TextStyle(fontSize = 16.sp)
+                        )
+                    }
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        // navigate back
+                    }) {
+                        Icon(Icons.AutoMirrored.Filled.KeyboardArrowLeft, contentDescription = null)
+                    }
+                },
+                actions = {
+                    Text(
+                        "Done",
+                        style = TextStyle(fontSize = 16.sp)
+                    )
+                }
+            )
+        }
+    ) { padding ->
+
+        val context = LocalContext.current
+        var imageBitmap = rememberSaveable {
+            mutableStateOf<ImageBitmap?>(null)
+        }
+        image?.let {
+            imageBitmap.value = it
+        }
+
+
+        val pickVisualMediaLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.PickVisualMedia()
+        ) { contentUri ->
+            contentUri?.let { uri ->
+                context.contentResolver.openFileDescriptor(uri, "r").use {
+                    it?.let { parcelFileDescriptor ->
+                        val fd: FileDescriptor = parcelFileDescriptor.fileDescriptor
+                        val bitmap: Bitmap = BitmapFactory.decodeFileDescriptor(fd)
+                        imageBitmap.value = bitmap.asImageBitmap()
+                    }
+                }
+            }
+        }
+
+        val permissionRequestLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                pickVisualMediaLauncher.launch(
+                    PickVisualMediaRequest(
+                        mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly
+                    )
+                )
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .background(Color.White)
+        ) {
+
+            if (imageBitmap.value != null) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                ) {
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Image(
+                        modifier = Modifier.fillMaxWidth(),
+                        bitmap = imageBitmap.value!!,
+                        contentDescription = null,
+                    )
+                    Spacer(modifier = Modifier.height(40.dp))
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.White),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        EditIcon(
+                            icon = R.drawable.brush,
+                            iconTitle = "Brush",
+                            onIconPressed = {}
+                        )
+                        EditIcon(
+                            icon = R.drawable.crop,
+                            iconTitle = "Crop",
+                            onIconPressed = {}
+                        )
+                        EditIcon(
+                            icon = R.drawable.rotate,
+                            iconTitle = "Rotate",
+                            onIconPressed = {}
+                        )
+
+                    }
+                }
+            } else {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                )
+            }
+
+            Button(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                onClick = {
+                    if (Utils.readPermissionsGranted(context)) {
+                        pickVisualMediaLauncher.launch(
+                            PickVisualMediaRequest(
+                                mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly
+                            )
+                        )
+                    } else {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                            permissionRequestLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                        } else {
+                            permissionRequestLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        }
+                    }
+                }
+            ) {
+                Text(text = "Pick Image")
+            }
+
+        }
+    }
+}
+
+@Composable
+fun EditIcon(
+    icon: Int,
+    iconTitle: String,
+    onIconPressed: () -> Unit
+) {
+    IconButton(
+        onClick = {
+            onIconPressed()
+        }
+    ) {
+        Column {
+            Icon(
+                painter = painterResource(icon),
+                contentDescription = null,
+                modifier = Modifier.size(28.dp)
+            )
+
+            Text(iconTitle, style = TextStyle(fontSize = 16.sp))
+        }
+    }
+}
+
+
+
+
+
---
+++ app/src/main/java/com/example/imageeditor/utils/Utils.kt
@@ -0,0 +1,23 @@
+package com.example.imageeditor.utils
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.content.ContextCompat
+
+object Utils {
+
+     fun readPermissionsGranted(context: Context): Boolean {
+        val readPermission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            Manifest.permission.READ_MEDIA_IMAGES
+        } else {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        }
+
+        return ContextCompat.checkSelfPermission(
+            context,
+            readPermission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+}
